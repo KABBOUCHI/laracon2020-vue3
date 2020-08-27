ea3aebe49bf24bbd24980e70758c783abaff51de
Georges KABBOUCHI <georges.kabbouchi@gmail.com>
2020-08-27 20:21:11 +0300

    Update app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,12 +1,10 @@
 import { InertiaApp } from '@inertiajs/vue/src'
 import { createApp, h } from 'vue'
 
-Vue.use(InertiaApp)
-
 const el = document.getElementById('app')
 
 const app = createApp({
-  render: h =>
+  render: () =>
     h(InertiaApp, {
       initialPage: JSON.parse(el.dataset.page),
       resolveComponent: name => require(`./Pages/${name}`).default,
